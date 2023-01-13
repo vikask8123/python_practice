7edd2e38293fe5c42fdf53858cec6bcdaab99cea
Vikas k <121590280+vikask8123@users.noreply.github.com>
2023-01-13 20:13:55 +0530

    Add files via upload

---
+++ module_2_Assignment.py
@@ -0,0 +1,30 @@
+#Q_1
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hi %s your age is %d" %(name,age))
+
+#Q_2
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hi {name} your age is {age}")
+
+#Q_3
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hi {} your age is {}".format(name,age))
+
+#Q_4
+
+number = 34
+float_number = 3.14
+city = "Bangalore"
+st_pass = True
+
+print(f"datatype of number is {type(number)}")
+print(f"datatype of float_number is {type(float_number)}")
+print(f"datatype of city is {type(city)}")
+print(f"datatype of st_pass is {type(st_pass)}")
+
+#Q_5
+city = "Banglore"
+print(city[0:4])
