f9e7dd5fc519ba167282c3f5da804b3bcc8b62ee
Vikas k <121590280+vikask8123@users.noreply.github.com>
2023-03-18 20:05:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import speech_recognition as amma
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+listener = amma.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with amma.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is' + time)
+        print(time)
+
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'what is ' in command:
+        talk('sorry i have a headache')
+    elif 'are you single ' in command:
+        talk('No! i am in relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif'':
+        talk('please say command again.')
+
+
+while True:
+    run_alexa()
+
+
+
